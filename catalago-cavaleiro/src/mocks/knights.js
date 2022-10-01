d79b7e48acb4048e2d9094af20ad79d3ed753d6c
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-10-01 16:05:50 -0300

    feat: create a structure of react

---
+++ catalago-cavaleiro/src/mocks/knights.js
@@ -0,0 +1,23 @@
+export const paletas = [
+  {
+      
+    "name": "Seiya",
+    "skill":"Meteoro de Pegasos",
+    "picture": "assets/images/seiya.png"
+    
+  },
+  {
+    
+    "name": "Ikki",
+    "skill":"Ave Fênix.",
+    "picture": "assets/images/ikki.png"
+    
+  },
+  {
+    
+    "name": "Hyoga",
+    "skill":"Pó de Diamante.",
+    "picture": "assets/images/hyoga.png"
+    
+  }
+]
